a4bb083906572c46b5af4abd09c8ff18669bb794
Jasminekhalimova <jasmine.khalimova@gmail.com>
2024-10-14 17:48:46 +0100

    implemented search on change e.g. which user typing

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,11 +4,14 @@ const Search = () => {
     // State to store fetched news articles
     const [news, setNews] = useState([]);
 
+    // Search query state
+    const [searchQuery, setSearchQuery] =  useState('');
+
     // Function to fetch news from the API
     const fetchNews = () => {
-        fetch("http://hn.algolia.com/api/v1/search?query=react")
+        fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
         .then(result => result.json())
-        .then(data => setNews(data.hits))  // Correct access to "hits"
+        .then(data => setNews(data.hits))
         .catch(error => console.log(error));
     };
 
@@ -16,11 +19,22 @@ const Search = () => {
     useEffect(() => {
         // Fetches news on component mount
         fetchNews();
-    }, []);  // Empty array ensures this runs only once
+    }, [searchQuery]);  // Empty array ensures this runs only once
+
+    //Handle change function
+    const handleChange = (e) => {
+        //update state depending what user enters
+        setSearchQuery(e.target.value)
+
+    };
 
     return (
         <div className="mt-4">
             <h2>Search Component</h2>
+            <form>
+                <input type="text" value={searchQuery} onChange={handleChange}></input>
+                <button>Search</button>
+            </form>
             {/* Map through the news array and render titles */}
             {news.map((n, i) => (
                 <p key={i}>{n.title}</p>
